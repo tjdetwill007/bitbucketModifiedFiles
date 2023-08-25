242a30d9525db78ba05bd4e8e67431f712375997
tjdetwill007 <tjdetwill007@gmail.com>
2023-08-26 00:09:19 +0530

    BitBucket_Changes

--- script.sh
+++ script.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 a=`git diff --name-only HEAD~ HEAD`
-if [ -n $a ];then
+echo $a
+if [[ -n $a ]];then
 	pwd=`pwd`
 	mkdir myfiles
 	for i in $a
 	do 
 		cd "$pwd"
+		echo "finding answers"
 		
-		absolute_path=`find ./ $i | grep "$i" | head -1 > absolute.text`
-		final_path=`sed -i "s/^\./""/" absolute.text`
+		absolute_path= `find ./ $i | grep -E "(^|\s)$i($|\s)" > absolute.text`
+		echo "Last was answer"
+		# absolute_path=`find ./ $i | grep "$i" | head -1 > absolute.text`
+		cat absolute.text
+		# final_path=`sed -i "s/^\./""/" absolute.text`
 		path=`cat absolute.text`
 		# echo $pwd > pwd.txt
 		# final_path1=`cat pwd.txt absolute.text | tr -d '\n'`
-		install -D ".$path" "myfiles$path"
+		install -D "./$path" "myfiles/$path"
 
 
 		
